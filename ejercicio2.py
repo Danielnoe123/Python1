09428516f885c99032626cf76b8762f6c45704bf
Daniel <60995677@continental.edu.pe>
2025-11-27 13:28:35 -0500

    Primer commit: agregar ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,15 @@
+# Programa: término n-ésimo de una progresión geométrica
+
+a1 = float(input("Ingresa el primer término (a1): "))
+r = float(input("Ingresa la razón (r): "))
+n = int(input("Ingresa el número de término que quieres (n): "))
+
+an = a1 * (r ** (n - 1))
+
+print("El término número", n, "de la progresión geométrica es:", an)
+if r != 1 :
+Sn = a1 * ( r**n-1 )/r+1
+print (f"el resultado es {Sn}")
+else :
+    Sn = a1 *n
+    print (f"el resultado es {Sn} ")
